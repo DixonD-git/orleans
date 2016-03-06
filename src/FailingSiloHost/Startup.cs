4855c56eb70b514caefdb7adc016af0f18b68922
Dmytro Dziuma <dixond@acm.lviv.ua>
2016-03-07 00:09:24 +0200

    Added Dev/Test host failing because of Microsoft.CodeAnalysis.CSharp.Scripting

---
+++ src/FailingSiloHost/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SiloHost1
+{
+    public class TestStartup
+    {
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            var t = typeof(CSharpScript);
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
